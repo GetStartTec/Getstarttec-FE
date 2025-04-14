35262bda86496b3708ddee3b00297bc105881b88
GetStartTec <getstarttec@gmail.com>
2025-04-14 11:33:58 +0530

    initail changes

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,9 +1,21 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Header } from "./Component/Header/Header";
+import { Contact } from "./Component/Contact/Contact";
+import { Footer } from "./Component/Footer/Footer";
+import { Home } from "./Component/Home/Home";
 
 function App() {
   return (
     <>
-    
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact-us" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+
     </>
   );
 }
